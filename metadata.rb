fc236bf1573898c8fff2654623ebc61e97d9204b
Richard Shade <rshade@rightscale.com>
2014-01-22 10:10:52 -0600

    adding attributes to metadata

--- metadata.rb
+++ metadata.rb
@@ -15,12 +15,61 @@ recipe "openvpn::default", "install openvpn base software, needed for both clien
 recipe "openvpn::lighttpd", "installs lighthttpd for serving certs"
 recipe "openvpn::server", "install openvpn server"
 
+#openvpn default
+attribute "openvpn/client/host_prefix",
+  :display_name => "OpenVPN Client Host Prefix",
+  :description => "OpenVPN Client Certificate Host Prefix",
+  :default => 'client', 
+  :required => "optional"
+
+attribute "openvpn/client/start_host_number",
+  :display_name => "OpenVPN Client First Host Number",
+  :description => "OpenVPN Client First Host Number",
+  :default => "1", 
+  :required => "optional"
+
+attribute "openvpn/client/count",
+  :display_name => "OpenVPN Client Count",
+  :description => "Number of OpenVPN Client Certs to Create",
+  :default => "20",
+  :required => "optional"
+  
+attribute "openvpn/client/domain",
+  :display_name => "OpenVPN Client Domain",
+  :description => "OpenVPN Client Domain",
+  :default => "example.com",
+  :required => "optional"
+
+#openvpn server
+attribute "openvpn/server/network_prefix",
+  :display_name => "OpenVPN Network Prefix",
+  :description => "OpenVPN Network Prefix",
+  :default => '192.168.1.0',
+  :required => "optional",
+  :recipes => [ "openvpn::server" ]
+
+attribute "openvpn/server/subnet_mask",
+  :display_name => "OpenVPN Subnet Mask",
+  :description => "OpenVPN Subnet Mask",
+  :default =>  '255.255.255.0',
+  :required => "optional",
+  :recipes => [ "openvpn::server" ]
+
+#openvpn client
 attribute "openvpn/server",
   :display_name => "OpenVPN Server",
   :description => "OpenVPN Server",
+  :required => "required",
   :recipes => [ "openvpn::client" ]
 
 attribute "openvpn/client/key_base_url",
   :display_name => "OpenVPN Client Package Base URL",
   :description => "OpenVPN Client Package Base URL",
+  :required => "required",
+  :recipes => [ "openvpn::client" ]
+
+attribute "openvpn/client/host_number",
+  :display_name => "OpenVPN Client Host Number",
+  :description => "OpenVPN Client Host Number",
+  :required => "required",
   :recipes => [ "openvpn::client" ]
