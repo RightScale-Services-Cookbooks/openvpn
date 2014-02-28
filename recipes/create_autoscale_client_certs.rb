e7fe9d872f81f7f3579d74bfb48493eabcf30fe2
Alex Pop <alex@rightscale.com>
2014-02-28 12:14:35 +0000

    Update and rename auto-scale-create-client.rb to create_autoscale_client_certs.rb

--- recipes/create_autoscale_client_certs.rb
+++ recipes/create_autoscale_client_certs.rb
@@ -1,7 +1,7 @@
 rightscale_marker :begin
 
 remote_recipe "create-server-cert" do
-  recipe "openvpn::client-cert-create"
+  recipe "openvpn::create_numbered_clinet_certs"
   attributes { openvpn/client/count => 254,  openvpn/client/count_start => 254 }
   recipients_tags ["provides:monkey_type=wind_up", "provides:monkey_type=golden"]
 end
