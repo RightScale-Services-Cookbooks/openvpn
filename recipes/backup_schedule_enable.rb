13b246004ee9c2dbf073ab77dcd8ac8b1c2f4bea
Alex Pop <alex@rightscale.com>
2014-03-03 10:55:50 +0000

    Update backup_schedule_enable.rb

--- recipes/backup_schedule_enable.rb
+++ recipes/backup_schedule_enable.rb
@@ -1,8 +1,18 @@
 rightscale_marker :begin
 
+bash "Installing apc" do
+  flags "-ex"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
 cat << EOF > /etc/cron.daily/vpn_backup
-#!/bin/bash
-command "rs_run_recipe --name 'backup_certificates' 2>&1 >> /var/log/rs_backup.log"
+#!/bin/sh
+rs_run_recipe --name 'openvpn::backup_certificates' 2>&1 >> /var/log/rs_backup.log
+exit 0
 EOF
+
+chmod +x /etc/cron.daily/vpn_backup
+  EOH
+end
 
 rightscale_marker :end
