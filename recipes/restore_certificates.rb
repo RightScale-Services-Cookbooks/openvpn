4752b87fb89b9abb06e1f9f911fcdadc46c67771
Alex Pop <alex@rightscale.com>
2014-03-03 13:49:58 +0000

    Update restore_certificates.rb

--- recipes/restore_certificates.rb
+++ recipes/restore_certificates.rb
@@ -1,5 +1,67 @@
-rightscale_marker :begin
+raise "*** ROS gem missing, please add rightscale::install_tools recipes to runlist." unless File.exists?("/opt/rightscale/sandbox/bin/ros_util")
 
-log "*** In restore #{node[:openvpn][:backup][:lineage]}, #{node[:openvpn][:backup][:storage_account_provider]}, #{node[:openvpn][:backup][:storage_account_endpoint]}, #{node[:openvpn][:backup][:container]}"
+log "*** In openvpn::restore_certificates"
 
-rightscale_marker :end
+if ("#{node[:openvpn][:certificates_action]}" != "Restore")
+  log "*** Skipping OpenVPN restore as attribute openvpn/certificates_action is not 'Restore'"
+  return
+end
+
+if File.directory?(node[:openvpn][:key_dir])
+  log "*** Skipping OpenVPN certificates restore as this folder already exists: #{node[:openvpn][:key_dir]}"
+else
+  log "*** #{node[:openvpn][:key_dir]} is missing, restoring it from ROS"
+  prefix = node[:openvpn][:backup][:lineage]
+  backupfilepath_without_extension = "/tmp/" + prefix
+  container = node[:openvpn][:backup][:container]
+  cloud = node[:openvpn][:backup][:storage_account_provider]
+  command_to_execute = "/opt/rightscale/sandbox/bin/ros_util get" +
+    " --cloud #{cloud} --container #{container}" +
+    " --dest #{backupfilepath_without_extension}" +
+    " --source #{prefix} --latest"
+
+  # Overrides default endpoint or for generic storage clouds such as Swift.
+  # Is set as ENV['STORAGE_OPTIONS'] for ros_util.
+  require 'json'
+
+  options =
+    if node[:openvpn][:backup][:storage_account_endpoint].to_s.empty?
+      {}
+    else
+      {'STORAGE_OPTIONS' => JSON.dump({
+        :endpoint => node[:openvpn][:backup][:storage_account_endpoint],
+        :cloud => node[:openvpn][:backup][:storage_account_provider].to_sym
+      })}
+    end
+
+  environment_variables = {
+    'STORAGE_ACCOUNT_ID' => node[:openvpn][:backup][:storage_account_id],
+    'STORAGE_ACCOUNT_SECRET' => node[:openvpn][:backup][:storage_account_secret]
+  }.merge(options)
+
+  # Obtain the dumpfile from ROS
+  execute "Download dumpfile from Remote Object Store" do
+    command command_to_execute
+    creates dumpfilepath_without_extension
+    environment environment_variables
+  end
+
+
+  bash "Restore keys tarball(#{dumpfilepath_without_extension})" do
+  flags "-ex"
+  code <<-EOH
+	  cd /
+    tar -xzf #{dumpfilepath_without_extension}
+  EOH
+end
+  
+  
+  # Delete the local file.
+  ruby_block "Delete the local file" do
+    block do
+      require "fileutils"
+      FileUtils.rm_f node[:openvpn][:backup][:filepath]
+    end
+  end
+
+end 
