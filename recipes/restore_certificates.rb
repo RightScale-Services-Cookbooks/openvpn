10d7237988d3c5b123046a75035829bbf83e73f7
Alex Pop <alex@rightscale.com>
2014-03-03 09:37:22 +0000

    Create restore_certificates.rb

---
+++ recipes/restore_certificates.rb
@@ -0,0 +1,5 @@
+rightscale_marker :begin
+
+log "*** In restore #{node[:openvpn][:backup][:lineage]}, #{node[:openvpn][:backup][:storage_account_provider]}, #{node[:openvpn][:backup][:storage_account_endpoint]}, #{node[:openvpn][:backup][:container]}"
+
+rightscale_marker :end
