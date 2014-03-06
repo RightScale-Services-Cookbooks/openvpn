da11e3d01552e4666c6d7299cd6d4cabdcd50b2b
Alex Pop <alex@rightscale.com>
2014-03-06 12:00:03 +0000

    Update client.rb

--- recipes/client.rb
+++ recipes/client.rb
@@ -27,7 +27,7 @@ template "/etc/openvpn/client.conf" do
              :subnet_mask => node[:openvpn][:server][:subnet_mask],
              :client_count => node[:openvpn][:client][:count],
              :server => node[:openvpn][:server],
-             :hostname => "#{node[:openvpn][:client][:cert_name]}.#{node[:openvpn][:cert][:cn]}"
+             :cert => node[:openvpn][:client][:cert_name]
             )
   action :create
 end
