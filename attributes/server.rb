80aaf1ad443baa27ba4035b9e991f19ad51828ad
Alex Pop <alex@rightscale.com>
2014-03-10 15:35:43 +0000

    add c2c attr

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,5 +1,6 @@
 default[:openvpn][:server][:network_prefix]='192.168.1.0'
 default[:openvpn][:server][:subnet_mask] = '255.255.255.0'
+default[:openvpn][:server][:c2c] = 'false'
 default[:openvpn][:client][:host_prefix] = 'client'
 default[:openvpn][:client][:start_host_number] = '1'
 default[:openvpn][:client][:count_start] = 0
