dff27ccff25fd0e710fcd156b78f259a09ad56a7
Alex Pop <alex@rightscale.com>
2014-03-10 09:44:22 +0000

    adding client cert inputs

--- recipes/client_input_certs.rb
+++ recipes/client_input_certs.rb
@@ -2,6 +2,21 @@ rightscale_marker :begin
 
 easy_rsa_dir="/etc/openvpn/easy-rsa/"
 
+file "#{easy_rsa_dir}keys/ca.crt" do
+  content node[:openvpn][:client][:ca_crt]
+  mode 00600
+end
+
+file "#{easy_rsa_dir}keys/client.crt" do
+  content node[:openvpn][:client][:client_crt]
+  mode 00600
+end
+
+file "#{easy_rsa_dir}keys/client.key" do
+  content node[:openvpn][:client][:client_key]
+  mode 00600
+end
+
 template "/etc/openvpn/client.conf" do
   source "client.conf.erb"
   owner "root"
@@ -16,7 +31,7 @@ template "/etc/openvpn/client.conf" do
              :subnet_mask => node[:openvpn][:server][:subnet_mask],
              :client_count => node[:openvpn][:client][:count],
              :server => node[:openvpn][:server],
-             :cert => node[:openvpn][:client][:cert_name]
+             :cert => "client"
             )
   action :create
 end
