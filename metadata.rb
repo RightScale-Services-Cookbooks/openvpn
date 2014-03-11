bf94e120a5092b09b0cdc51ffa416d39e633b3e1
Alex Pop <alex@rightscale.com>
2014-03-11 14:22:09 +0000

    increase minor rev after publishing

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@rightscale.com'
 license          'Apache 2.0'
 description      'Installs/Configures openvpn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.1'
+version          '1.1.2'
 
 depends "rightscale"
 depends "sys_firewall"
